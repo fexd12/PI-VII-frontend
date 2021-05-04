f9898fcaf3588ce255eeb5d47aae4ec3e02aad45
felipe amorim <29824714+fexd12@users.noreply.github.com>
2021-05-04 00:50:38 -0300

    adicionando geracao de qrcode

--- src/views/GeradorQrcode.vue
+++ src/views/GeradorQrcode.vue
@@ -1,32 +1,49 @@
 <template>
   <div>
     <SideMenu></SideMenu>
+    <div class="container">
+        <label for="">Cod. Envio:{{id_envio}}</label>
+        <img  v-bind:src="'data:image/png;base64,'+img" alt="qrcode">
+    </div>
   </div>
 </template>
 
 <script>
-import { signOut, isSignedIn } from "../auth";
 import SideMenu from "../components/SideMenu.vue";
 
 export default {
   components: {
     SideMenu,
   },
+  data:()=>{
+      return { 
+          img:'',
+          id_envio:'1234'
+      }
+  },
   methods: {
-    logout() {
-      signOut();
-      this.$router.push("/login");
-    },
-    async authenticate() {
-      let signed = await isSignedIn(this.$baseUrl);
-      if (!signed) this.$router.push("/login");
-    },
+      async getQrCode(){
+          try {
+            let response = await this.$http.post(`${this.$baseUrl}/envio/qrcode`,{id_envio:this.id_envio});
+            let img = response.data.img[0];
+            console.log(img)
+            this.img = img
+
+         } catch (error) {
+              console.log('error:'+error)
+          }
+      }
   },
-  async mounted() {
-    await this.authenticate();
-  },
+  async mounted(){
+      await this.getQrCode()
+  }
+
 };
 </script>
 
 <style>
+.container{
+  width: 600px !important;
+  display: row !important;
+}
 </style>
