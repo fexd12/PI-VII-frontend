d4bc43e9c62a706db2646fa29870ecbea1e3381e
felipe amorim <29824714+fexd12@users.noreply.github.com>
2021-05-04 20:48:27 -0300

    adicionado scanner qrcode

--- src/views/Scanner.vue
+++ src/views/Scanner.vue
@@ -1,18 +1,27 @@
 <template>
 <div>
   <SideMenu></SideMenu>
+  <Qrcoded @decoded="onDecode"/>
 </div>
 </template>
 
 <script>
 import SideMenu from '../components/SideMenu.vue';
-
+import Qrcoded from '../components/ComponenteGeradorQrcode'
 export default {
   components: {
     SideMenu,
+    Qrcoded
+  },
+  data:() => {
+    return{
+        value:''
+    }
   },
   methods: {
-
+    onDecode(decoded){
+        console.log(decoded)
+    }
   },
   async mounted(){
   }
